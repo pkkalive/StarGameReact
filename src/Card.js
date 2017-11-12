a59b987bde2c51bcd33646d18fb3ae8fcedeec90
Kalive Purushotham Kumar <pkkalive@users.noreply.github.com>
2017-11-12 17:15:00 +0530

    understanding card creation app

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div>
+      <img src="http://placehold.it/75" />
+      <div>
+        <div>Name goes here .... </div>
+        <div>Company goes here .... </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import App from './App';
+import Card from './Card';
 
-render(<App />, document.getElementById('react-root'));
+render(<Card />, document.getElementById('react-root'));
