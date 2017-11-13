1222fd622d97fde72bb85c88ee92c62c701730f1
Kalive Purushotham Kumar <pkkalive@users.noreply.github.com>
2017-11-13 15:21:21 +0530

    removed all and started building game

---
+++ src/Game.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Game extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Play Nine</h3>
+      </div>
+    );
+  }
+}
