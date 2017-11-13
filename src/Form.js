cd94ec6fbf7f7c1670040708c55d218b02fe580f
Kalive Purushotham Kumar <pkkalive@users.noreply.github.com>
2017-11-13 12:17:42 +0530

    pulling information from githubapi

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class Form extends Component {
+  state = {
+    userName:''
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+      .then(resp => {
+        console.log(resp);
+      });
+  };
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Github User Name" />
-        <button type="submit">Add Card</button> 
+      <form onSubmit={this.handleSubmit}>
+        <input type="text"
+              value = {this.state.userName}
+              onChange={(event) => this.setState({userName: event.target.value})}
+              placeholder="Github User Name" required/>
+        <button type="submit">Add Card</button>
       </form>
     );
   }
